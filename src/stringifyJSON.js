f1f3969606d054f001f38bf091dd8c5b3735174e
Wilson Li <wilsonli415@gmail.com>
2018-09-09 15:08:06 -0700

    Commit StringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,44 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var string = ''
+  //obj.forEach(function(ele) {
+
+    // Primitive Types
+  if (typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === 'symbol') {
+    string += obj.toString();
+  } else if (obj === null) {
+    string += obj;
+  } else if (typeof obj === 'string') {
+    string += '"' + obj + '"';
+  }
+    // Array Types
+  if (Array.isArray(obj)) {
+    string += '[';
+    obj.forEach(function(ele, i, arr) {
+      string += stringifyJSON(ele);
+      if (i !== obj.length - 1) {
+        string += ',';
+      } 
+    })
+    string += ']';
+  }
+    // Object Types
+  else if (obj instanceof Object) {
+    string += '{'; 
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+        string += '';    //skip or do nothing
+      } else {
+        string += stringifyJSON(key) + ':';
+        string += stringifyJSON(obj[key]);
+        if (key !== Object.keys(obj)[Object.keys(obj).length - 1]) {
+          string += ',';
+        } 
+      }
+    }
+    string += '}';
+  }
+  return string;
 };
+
