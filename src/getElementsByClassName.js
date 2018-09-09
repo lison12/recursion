32000226997b943ee379741ad9ebac316a9067c6
Wilson Li <wilsonli415@gmail.com>
2018-09-09 15:06:49 -0700

    Commit getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var resultsWithClass = [];
+  var docBod = document.body;
+
+  var iterateBody = function(element) {
+    // If body element contains class name
+    if (element.classList && element.classList.contains(className)) {
+      resultsWithClass.push(element);
+    }
+    // If body element contains child nodes
+    if (element.childNodes) {
+      let cNodes = element.childNodes;
+      for (var i = 0; i < cNodes.length; i++) {
+        iterateBody(cNodes[i]);
+      }
+    }
+  };
+
+  iterateBody(document.body);
+  return resultsWithClass;
 };
+
+//document.body, element.childNodes, and element.classList
